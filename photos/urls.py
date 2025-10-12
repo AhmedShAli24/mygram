5b7b6d11c7a54511da7f2da1cb25dd75a0c6e6e6
Ahmed Ali <ahmed24481@gmail.com>
2025-10-12 12:39:14 -0600

    Implemented search. Able to search usernames and look at thier photos

--- photos/urls.py
+++ photos/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("<int:photo_id>", views.details, name="details"),
     path("upload", views.upload_photo, name="upload_photo"),
-    path('my_photos', views.my_photos, name='my_photos')
+    path('my_photos', views.my_photos, name='my_photos'),
+    path('search', views.search, name='search')
 ]
--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 from .models import Photo
 from .forms import PhotoForm
@@ -37,3 +38,31 @@ def my_photos(request):
 def details(request, photo_id):
     photo = Photo.objects.get(pk=photo_id)
     return HttpResponse("you got the photo %s" % photo.caption)
+
+def search(request):
+    User = get_user_model()
+    searched = request.GET.get('searched', '')
+    if searched:
+        """
+        select u.*
+        from users u
+        where u.username ilike '%ahmed%'
+        limit 1;
+        """
+        searched_user = User.objects.filter(username__icontains = searched)
+        """
+        select p.*
+        from photos p
+        left join users u on (u.id = p.user_id)
+        where u.username ilike '%ahmed%';
+        """
+        photos = Photo.objects.filter(user__username__icontains=searched)
+        
+        print("=====================")
+        print(searched_user.query)
+        print("=====================")
+        print(photos.query)
+        print("=====================")
+        
+    return render(request, 'photos/searched.html',  {'searched_user': searched_user.first(), 'photos': photos} )
+        
