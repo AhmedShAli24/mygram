b98dbfa9c9a29e50b71a8cb76c5351adaf6dacf0
Ahmed Ali <ahmed24481@gmail.com>
2025-11-12 12:23:47 -0700

    Implemented comments into the project. are now able to add comments to each photo.

--- photos/urls.py
+++ photos/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("<int:photo_id>", views.details, name="details"),
     path("upload", views.upload_photo, name="upload_photo"),
     path('my_photos', views.my_photos, name='my_photos'),
-    path('search', views.search, name='search')
+    path('search', views.search, name='search'),
+    path('comment/<int:photo_id>/add/', views.comments, name='comments')
 ]
--- photos/views.py
+++ photos/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 from django.urls import reverse
-from .models import Photo, UserPhotoLikes
+from .models import Photo, UserPhotoLikes, UserComments
 from .forms import PhotoForm
 
 
@@ -42,7 +42,9 @@ def details(request, photo_id):
 
 def search(request):
     User = get_user_model()
+    print(User)
     searched = request.GET.get('searched', '')
+    print(searched)
     
     searched_user = User.objects.filter(username__icontains = searched).first()
     photos = Photo.objects.filter(user__username__icontains=searched)
@@ -54,18 +56,15 @@ def search(request):
         print("+++++++++++++++++")
         print(user_id)
         
-        
         ## if it doesn't exist in UserPhotoLike create it
-        userlike = UserPhotoLikes.objects.filter(user = user_id, photo = photo_id).first()
-        print(userlike)
-        if userlike:
-            userlike.delete()
+        userliked = UserPhotoLikes.objects.filter(user = user_id, photo = photo_id).first()
+        print(userliked)
+        if userliked:
+            userliked.delete()
         else:
             new_user_like = UserPhotoLikes(user_id = user_id, photo_id = photo_id)
             new_user_like.save()
             print(new_user_like)
-        
-       
         
         # photo = get_object_or_404(Photo, id=photo_id)
         # print(photo)
@@ -79,8 +78,31 @@ def search(request):
         print("################")
         print(search_url)
         return redirect(f"{search_url}?searched={searched}")
+    
+    #   
+    user_list_of_photo = UserPhotoLikes.objects.filter(user = request.user)
+    list_of_user_likes = set()
+    for userPhoto in user_list_of_photo:
+        print(userPhoto.photo.id)
+        list_of_user_likes.add(userPhoto.photo.id)
         
-    return render(request, 'photos/searched.html',  {'searched_user': searched_user, 'photos': photos, 'searched': searched })
+   
+    return render(request, 'photos/searched.html',  {
+        'searched_user': searched_user,
+        'photos': photos,
+        'searched': searched,
+        'list_of_user_likes': list_of_user_likes
+    })
 
 
+def comments(request, photo_id):
+    if request.method == 'POST':
+        comment_text = request.POST.get('comment_text')
+        search = request.POST.get('searched')
+        
+        photo = Photo.objects.get(id = photo_id)
+        
+        if comment_text:
+            UserComments.objects.create(user = request.user, photo = photo, comment = comment_text)
+        return redirect(f"{reverse('photos:search')}?searched={search}")
         
